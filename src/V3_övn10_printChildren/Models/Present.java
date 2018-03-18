071872667d3e782518c599c43da883a5a2c66e08
Sigrun Olafsdottir <solafsdottir@gmail.com>
2018-03-18 11:29:22 +0100

    Demos v 3 added

---
+++ src/V3_övn10_printChildren/Models/Present.java
@@ -0,0 +1,23 @@
+package V3_övn10_printChildren.Models;
+
+public class Present {
+
+    private int id;
+    private String name;
+    
+    public Present(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    
+    public Present(){}
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
